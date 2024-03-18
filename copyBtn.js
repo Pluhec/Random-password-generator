e7c26fb964752a1f8262c09ab85b5c0ce0b1c27a
Pluhec <jakplu@seznam.cz>
2024-03-18 11:47:14 +0100

    Project Done

--- copyBtn.js
+++
@@ -1,6 +0,0 @@
-var copybtn = document.getElementById("copy-btn")
-var password = document.getElementById("generated-password")
-
-copybtn.onclick = function(){
-    navigator.clipboard.writeText(password.innerHTML)
-}
---
+++ main.js
@@ -0,0 +1,55 @@
+/******
+Generator
+******/
+function generatePassword() {
+  let x = document.getElementById("rangeNumber").value;
+  let pass = "";
+  let str =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "abcdefghijklmnopqrstuvwxyz0123456789@#$";
+
+  for (let i = 1; i <= x; i++) {
+    let char = Math.floor(Math.random() * str.length);
+    pass += str.charAt(char);
+  }
+
+  document.getElementById("generated-password").innerText = pass;
+}
+
+/******
+Copy button
+******/
+var copybtn = document.getElementById("copy-btn")
+var password = document.getElementById("generated-password")
+
+copybtn.onclick = function(){
+    navigator.clipboard.writeText(password.innerHTML)
+    copybtn.innerHTML = "Copied"
+    setTimeout(function(){
+        copybtn.innerHTML = "Copy"
+    }, 2000)
+}
+
+/******
+Slider
+******/
+const allRanges = document.querySelectorAll(".range-wrap");
+allRanges.forEach(wrap => {
+  const range = wrap.querySelector(".range");
+  const bubble = wrap.querySelector(".bubble");
+
+  range.addEventListener("input", () => {
+    setBubble(range, bubble);
+  });
+  setBubble(range, bubble);
+});
+
+function setBubble(range, bubble) {
+  const val = range.value;
+  const min = range.min ? range.min : 0;
+  const max = range.max ? range.max : 100;
+  const newVal = Number(((val - min) * 100) / (max - min));
+  bubble.innerHTML = val;
+
+  // Sorta magic numbers based on size of the native UI thumb
+  bubble.style.left = `calc(${newVal}% + (${8 - newVal * 0.15}px))`;
+}
